d78c4a9dbc6fa9e2c9c1b6640f39863c7d177cb0
chito529 <juanalmiron529@gmail.com>
2026-02-09 00:51:16 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css'; // Esto importa las directivas de Tailwind que pusiste en index.css
+import './index.css';
 
-const container = document.getElementById('root');
-
-if (!container) {
-  throw new Error('No se encontró el elemento root. Verifica tu index.html.');
-}
-
-const root = createRoot(container);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = createRoot(document.getElementById('root')!);
+root.render(<App />);
